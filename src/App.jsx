58938757f9386896868990d5db006c5d5e4f9f89
Bushra Salama <bushra.salama@gmail.com>
2025-11-18 14:37:07 -0600

    Challenge 2: Implement Blog Post Viewing

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,34 @@
 import './App.css'
 import BlogPostList from './BlogPostList'
 
-
 const posts = [
     {
     id: '1',
     title: 'Getting Started with React',
     summary: 'Learn the basics of React and build your first application.',
     formattedDate: 'January 1, 2023',
-    url: '/post',
+    author: "Bushra Salama",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque aperiam quia sit qui neque totam sunt? Dolor est hic ipsum recusandae officia cumque nostrum fuga expedita? Nobis magnam at dicta?",
+    url: '/post1',
     },
     {
     id: '2',
     title: 'CSS Grid vs. Flexbox',
     summary: 'A comparison of two powerful layout systems in CSS.',
+    author: "Bushra Salama",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque aperiam quia sit qui neque totam sunt? Dolor est hic ipsum recusandae officia cumque nostrum fuga expedita? Nobis magnam at dicta?",
     formattedDate: 'February 15, 2023',
-    url: '/post',
+    url: '/post2',
     },
+
     {
     id: '3',
     title: 'Accessibility in Web Development',
     summary: 'Tips for making your web applications more accessible.',
+    author: "Bushra Salama",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque aperiam quia sit qui neque totam sunt? Dolor est hic ipsum recusandae officia cumque nostrum fuga expedita? Nobis magnam at dicta?",
     formattedDate: 'March 10, 2023',
-    url: '/post',
+    url: '/post3',
     },
 ]
 
@@ -31,7 +37,7 @@ function App() {
 
   return (
     <main>
-      <h1>Hello, World!</h1>
+      <h1>Blog Posts!</h1>
         <BlogPostList posts={posts} />
     </main>
   )
